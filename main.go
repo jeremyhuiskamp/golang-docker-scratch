026ddd6232ecf1f28974f404d6b8247b9d2cf4df
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-05-15 21:42:07 +0200

    error exit on failure

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -10,25 +11,30 @@ import (
 // loading a timezone and verifying a well-known TLS certificate.
 func main() {
 	fmt.Println("\nTests from within scratch container:")
-	testTZ()
-	testTLS()
+	errors := testTZ()
+	errors += testTLS()
+	os.Exit(errors)
 }
 
-func testTZ() {
+func testTZ() (errors int) {
 	berlin, err := time.LoadLocation("Europe/Berlin")
 	if err != nil {
 		fmt.Printf("Unable to load timezones: %s\n", err)
+		errors++
 	} else {
 		fmt.Printf("Successfully loaded %q\n", berlin)
 	}
+	return
 }
 
-func testTLS() {
+func testTLS() (errors int) {
 	rsp, err := http.Get("https://google.de")
 	if err != nil {
 		fmt.Printf("Unable to establish https connection: %s\n", err)
+		errors++
 	} else {
 		rsp.Body.Close()
 		fmt.Println("Successfully established https connection")
 	}
+	return
 }
